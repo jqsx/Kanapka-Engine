fa56772cdbf88546e2ed1d079f534ceba1626cdd
jqsx <101830653+jqsx@users.noreply.github.com>
2024-02-05 18:05:31 +0100

    a

--- src/main/java/KanapkaEngine/Components/Renderer.java
+++ src/main/java/KanapkaEngine/Components/Renderer.java
@@ -4,13 +4,19 @@ import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
-public abstract class Renderer extends NodeComponent {
+public class Renderer extends NodeComponent {
     BufferedImage rendered_visual;
     int loading_state = NOT_STARTED;
-    public abstract BufferedImage getRender();
+
+    protected BufferedImage getRender() {
+        return null;
+    }
 
     public void setTexture(BufferedImage image) {
         rendered_visual = image;
+    }
+    public final int getRenderState() {
+        return loading_state;
     }
     public final static int NOT_STARTED = 0;
     public final static int STARTED = 1;
