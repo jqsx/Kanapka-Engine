1cffb72e0e303a0bb75710dd6d7f64eecf674c42
jqsx <101830653+jqsx@users.noreply.github.com>
2024-02-13 21:47:40 +0100

    created TextureAnimation class

---
+++ src/main/java/KanapkaEngine/Components/TextureAnimation.java
@@ -0,0 +1,17 @@
+package KanapkaEngine.Components;
+
+import java.awt.image.BufferedImage;
+
+public class TextureAnimation {
+    public TextureAnimation(String spriteSheetPath, int resolution) {
+
+    }
+
+    public void defineAnim(int row, String animationName) {
+
+    }
+
+    public BufferedImage getAnimationFrame(String name, int frame) {
+        return null;
+    }
+}
