49124f447ebfc8c2e64ce0aa883dbea6603f4cd7
jqsx <101830653+jqsx@users.noreply.github.com>
2025-03-08 15:50:50 +0100

    TrackedData class update

--- src/main/java/KanapkaEngine/Net/TrackedData.java
+++ src/main/java/KanapkaEngine/Net/TrackedData.java
@@ -2,6 +2,28 @@ package KanapkaEngine.Net;
 
 import KanapkaEngine.Game.WIP;
 
+/**
+ * A class activated from network components (to be added) to allow for easier tracking of data in multiplayer
+ * Should be able to support some utility classes and will attempt to serialize data into simplistic other components to be reconstructed later.
+ */
 @WIP
-public class TrackedData {
+public final class TrackedData<T> {
+    private T stored;
+
+    public TrackedData() {
+        stored = null;
+    }
+    public TrackedData(T init) {
+        stored = init;
+    }
+
+    public void set(T v) {
+        this.stored = v;
+
+        // Sync message
+    }
+
+    public T get() {
+        return stored;
+    }
 }
