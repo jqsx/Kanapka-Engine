8e6788bcc99134df8ca6fa221eea8ac9fe038541
jqsx <101830653+jqsx@users.noreply.github.com>
2025-03-18 12:36:43 +0100

    Added repeating tasks

--- src/main/java/KanapkaEngine/Game/Scheduler.java
+++ src/main/java/KanapkaEngine/Game/Scheduler.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public final class Scheduler implements Plugin {
     private static final List<delayed> list = new ArrayList<>();
+    private static final List<RepeatingTask> repeat = new ArrayList<>();
 
     Scheduler() {
 
@@ -20,6 +21,7 @@ public final class Scheduler implements Plugin {
     @Override
     public void Update() {
         list.removeIf(delayed::execute);
+        repeat.forEach(RepeatingTask::exec);
     }
 
     @Override
@@ -29,6 +31,16 @@ public final class Scheduler implements Plugin {
 
     public static void delay(Runnable task, double delay) {
         list.add(new delayed(task, delay));
+    }
+
+    public static RepeatingTask repeating(Runnable task, double every) {
+        RepeatingTask rt;
+        repeat.add(rt = new RepeatingTask(task, every));
+        return rt;
+    }
+
+    public static void endRepeating(RepeatingTask repeatingTask) {
+        repeat.remove(repeatingTask);
     }
 
     static class delayed {
@@ -49,6 +61,27 @@ public final class Scheduler implements Plugin {
             }
 
             return false;
+        }
+    }
+
+    public static class RepeatingTask {
+        private final Runnable task;
+        private final double every;
+
+        private double last;
+
+        private RepeatingTask(Runnable task, double every) {
+            this.task = task;
+            this.every = every;
+
+            last = Time.time();
+        }
+
+        private void exec() {
+            if (last + every < Time.time()) {
+                task.run();
+                last = Time.time();
+            }
         }
     }
 }
